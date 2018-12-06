207352788ec6d68cf75c6e032d943fc5b2535304
brinkqiang <brink.qiang@gmail.com>
2018-12-06 08:49:01 +0800

    add

--- src/stacktrace.h
+++ src/stacktrace.h
@@ -22,13 +22,13 @@ struct stacktrace *_stacktrace_get_exc();
 
 struct stacktrace *stacktrace_get_exc();
 
-extern "C" void __cxa_throw(void *thrown_exception, std::type_info *tinfo, void(*dest)(void *))
-__attribute__((noreturn));
-
-extern "C" void __wrap___cxa_throw(void *thrown_exception, std::type_info *tinfo, void(*dest)(void *)) {
-    _stacktrace_set_exc();
-    __cxa_throw(thrown_exception, tinfo, dest);
-}
+//extern "C" void __cxa_throw(void *thrown_exception, std::type_info *tinfo, void(*dest)(void *))
+//__attribute__((noreturn));
+//
+//extern "C" void __wrap___cxa_throw(void *thrown_exception, std::type_info *tinfo, void(*dest)(void *)) {
+//    _stacktrace_set_exc();
+//    __cxa_throw(thrown_exception, tinfo, dest);
+//}
 
 void stacktrace_string(struct stacktrace *trace, std::string* str);
 
