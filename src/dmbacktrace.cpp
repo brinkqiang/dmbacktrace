72fe2f8a0af80255967c4217b9ef47c2385d3687
brinkqiang <brink.qiang@gmail.com>
2018-12-06 10:05:00 +0800

    add

--- src/dmbacktrace.cpp
+++ src/dmbacktrace.cpp
@@ -119,7 +119,7 @@ std::string prettyBackTrace(int skipframes)
 
     for (j = 0; j < nptrs; j++)
     {
-        std::string strLine = fmt::format("{0:02} {}\n", j, strings[j]);
+        std::string strLine = fmt::format("{0:02} {1}\n", j, strings[j]);
         strTrace.append(strLine);
         printf("[%02d] %s\n", j, strings[j]);
     }
